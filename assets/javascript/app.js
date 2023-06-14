b73d83189c68790da4ccfdc2da17e2e8589911a8
shantdashjian <shantgdashjian@gmail.com>
2023-06-14 17:05:52 +0400

    [#28] Display current time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const currentLocalTimeEl = document.getElementById('current-local-time')
+
+setInterval(() => {
+	const currentLocalTime = new Date().toLocaleTimeString()
+	currentLocalTimeEl.textContent = `Current Local Time: ${currentLocalTime}`
+}, 1000)
