ae93339929fa1e3572b50cc8e2363028c9b56ff3
Kevin Do <kietdo172@gmail.com>
2023-06-15 19:41:12 -0400

    add my infor and delete kevin.html

--- assets/javascript/contributors.js
+++ assets/javascript/contributors.js
@@ -4,4 +4,9 @@ const contributors = [
         location: "Austin, TX, USA",
         favorite_ice_cream: "dark chocolate"
     },
+    {
+        name: "Kevin Do",
+        location: "Florida, Fl, USA",
+        favorite_ice_cream: "Milk chocolate"
+    },
 ]
