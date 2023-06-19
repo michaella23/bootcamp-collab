c5778fc80fd5c38fd9dfd44534cfb83ee11d858d
William S <will.step.205@gmail.com>
2023-06-18 18:24:48 -0700

    major refactoring theme logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,39 +6,33 @@ console.log(greetingEl);
 
 setInterval(() => {
     const currentLocalTime = new Date().toLocaleTimeString();
+
+    // show current time
+    currentLocalTimeEl.textContent = `Local Time: ${currentLocalTime}`;
+}, 1000)
+
+
+const themeForDaytime = () => {
     // the user is saving the date object to a variable
-	const date = new Date();
+    const date = new Date();
     // then pulling the hours from the date object
     const hours = date.getHours();
-    
-    /* 
-        user is declaring an undefined variable outside
-        the scope of the control structure / if statement 
-        below and then setting the value depending on
-        the hours.
-    */
-	let timeOfDay;
-    let timeOfDayEmote;
 
-	if (hours < 12) {
-        timeOfDay = "morning";
-        timeOfDayEmote = '🌄';
-    } else if (hours >= 12 && hours < 17) {
-        timeOfDay = "afternoon";
-        timeOfDayEmote = '🌅';
-    } else {
-        timeOfDay = "night";
-        timeOfDayEmote = '🌆';
+    // switch to night theme after 5pm
+    if (hours > 17) {
         document.body.style.color = "silver";
         document.body.style.background = "var(--clr-maroon-overload-will-s-205)";
     }
-
-    currentLocalTimeEl.textContent = `Local Time: ${currentLocalTime}`;
-    greetingEl.textContent = `Good ${timeOfDay} ${timeOfDayEmote} bootcamp students!`
-}, 1000)
+}
+themeForDaytime();
 
 function setTheme() {
-    const hours = new Date().getHours();
+    // the user is saving the date object to a variable
+    const date = new Date();
+    // then pulling the hours from the date object
+    const hours = date.getHours();
+
+    // toggle theme of the webpage
     if (hours < 17 && document.getElementById("theme-button").textContent == "THEME") {
         document.body.style.color = "silver";
         document.body.style.background = "var(--clr-maroon-overload-will-s-205)"; //DARK
@@ -56,4 +50,5 @@ function setTheme() {
         document.body.style.background = "var(--clr-background-tint)"; // LIGHT
         document.getElementById("theme-button").textContent = "LIGHT"; // LIGHT
     }
+
 }
