a084cc2d47ceed41660a189baf2546ad4d08b217
awcoetzer <83278375+awcoetzer@users.noreply.github.com>
2023-06-20 12:04:01 +0300

    feat: #27 #20 #34 contribution
    Solved issue #27, by rendering a people in the contributors.js file
    to the page
    Solved issue #20 Add a list of why you think scrimba is awesome, by
    allowing contributors to add a comment to the contributors.js file and
    then render it out on the page
    Solved issue #34 add info to contributors.js file

---
+++ assets/javascript/contributors-component.js
@@ -0,0 +1,209 @@
+'use strict';
+
+import { contributors } from "./contributors.js";
+
+const scrimbaContributorsContainerEl = document.querySelector('.scrimba-contributors--container');
+const scrimbaContributorsLoadMoreBtnEl = document.querySelector('.load-more');
+
+let loadAmount, loadCount;
+
+/*
+    The function below generates a random number for
+    a random default profile image
+*/
+const randomDefaultProfilePic = function () {
+  const randomNumber = Math.floor(Math.random() * 10);
+
+  return `./assets/images/scrimba-likes-default/default-${randomNumber}.jpg`
+}
+
+/*
+    Below are a bunch of bite sized create... Element functions
+    that just create elements, add classes/attributes/content
+*/
+const createScrimbaContributorsCardEl = function () {
+  const scrimbaContributorsCardEl = document.createElement('div');
+  scrimbaContributorsCardEl.classList.add('scrimba-contributors-card');
+  return scrimbaContributorsCardEl;
+}
+
+const createUserImgBoxEl = function () {
+  const userImgBoxEl = document.createElement('div');
+  userImgBoxEl.classList.add('scrimba-contributors--user-img-box');
+
+  return userImgBoxEl;
+}
+
+const createUserImgEl = function (index) {
+  const userImgEl = document.createElement('img');
+  const ifUserImage = contributors[index].profileImage ? contributors[index].profileImage : randomDefaultProfilePic();
+  userImgEl.classList.add('scrimba-contributors--user-img');
+  userImgEl.setAttribute('src', ifUserImage);
+
+  return userImgEl;
+}
+
+const createUserTextBoxEl = function () {
+  const userTextBoxEl = document.createElement('div');
+  userTextBoxEl.classList.add('scrimba-contributors--user-text-box');
+
+  return userTextBoxEl;
+}
+
+const createUserTextBoxHeaderEl = function () {
+  const userTextBoxHeaderEl = document.createElement('div');
+  userTextBoxHeaderEl.classList.add('user-text-box-header');
+
+  return userTextBoxHeaderEl;
+}
+
+const createUsernameContainerEl = function () {
+  const usernameContainerEl = document.createElement('div');
+  usernameContainerEl.classList.add('username-container');
+
+  return usernameContainerEl;
+}
+
+const createUsernameEl = function (index) {
+  const usernameEl = document.createElement('span');
+  usernameEl.classList.add('scrimba-contributors--username');
+  usernameEl.textContent = contributors[index].name;
+
+  return usernameEl;
+}
+
+const createUserLinkEl = function (index) {
+  const userLinkEl = document.createElement('a');
+  userLinkEl.classList.add('scrimba-contributors--link');
+  userLinkEl.setAttribute('href', contributors[index].githubLink);
+  userLinkEl.setAttribute('target', '_blank');
+  userLinkEl.textContent = contributors[index].githubName;
+
+  return userLinkEl;
+}
+
+const createFavIcecreamEl = function (index) {
+  const favIcecreamEl = document.createElement('span');
+  favIcecreamEl.classList.add('scrimba-contributors--fav-icecream');
+  favIcecreamEl.textContent = `My favorite ice-cream is ${contributors[index].favorite_ice_cream}`;
+
+  return favIcecreamEl;
+}
+
+const createLocationEl = function (index) {
+  const locationEl = document.createElement('span');
+  locationEl.classList.add('scrimba-contributors--location');
+  locationEl.textContent = contributors[index].location;
+
+  return locationEl;
+}
+
+const createUserCommentEl = function (index) {
+  const userCommentEl = document.createElement('p');
+  userCommentEl.classList.add('scrimba-contributors--user-comment');
+  userCommentEl.textContent = contributors[index].likes;
+
+  return userCommentEl;
+}
+
+const createMoreContributorsElements = function () {
+  loadCount = document.querySelectorAll('.scrimba-contributors-card').length;
+  for (let i = loadCount; i < loadAmount + loadCount; i++) {
+    if (i < contributors.length) {
+      createScrimbaContributorsElements(i);
+    }
+  }
+  loadCount = document.querySelectorAll('.scrimba-contributors-card').length;
+}
+
+const checkLoadCount = function () {
+  if (loadCount >= contributors.length) {
+    scrimbaContributorsLoadMoreBtnEl.classList.add('hide')
+  }
+}
+
+/*
+    Below are a bunch of append element functions
+*/
+const appendContentsUserImgBoxEl = function (userImgBoxEl, userImgEl) {
+  userImgBoxEl.append(userImgEl);
+}
+
+const appendUsernameContainerEl = function (index, usernameContainerEl, usernameEl, userLinkEl, favIcecreamEl) {
+  if (contributors[index].githubLink) {
+    usernameContainerEl.append(usernameEl, userLinkEl, favIcecreamEl);
+  } else {
+    usernameContainerEl.append(usernameEl, favIcecreamEl);
+  }
+}
+
+const appendUserTextBoxHeaderEl = function (userTextBoxHeaderEl, usernameContainerEl, locationEl) {
+  userTextBoxHeaderEl.append(usernameContainerEl, locationEl);
+}
+
+const appendUserTextBoxEl = function (userTextBoxEl, userTextBoxHeaderEl, userCommentEl) {
+  userTextBoxEl.append(userTextBoxHeaderEl, userCommentEl);
+}
+
+const appendScrimbaContributorsCardEl = function (scrimbaContributorsCardEl, userImgBoxEl, userTextBoxEl) {
+  scrimbaContributorsCardEl.append(userImgBoxEl, userTextBoxEl);
+}
+
+const appendScrimbaContributorsContainerEl = function (scrimbaContributorsCardEl) {
+  scrimbaContributorsContainerEl.append(scrimbaContributorsCardEl);
+}
+
+/*
+    This function puts it all together, so that we can run this
+    in a loop below this function
+*/
+const createScrimbaContributorsElements = function (index) {
+  const scrimbaContributorsCardEl = createScrimbaContributorsCardEl();
+  const userImgBoxEl = createUserImgBoxEl();
+  const userImgEl = createUserImgEl(index);
+  const userTextBoxEl = createUserTextBoxEl();
+  const userTextBoxHeaderEl = createUserTextBoxHeaderEl();
+  const usernameContainerEl = createUsernameContainerEl();
+  const usernameEl = createUsernameEl(index);
+  const userLinkEl = createUserLinkEl(index);
+  const favIcecreamEl = createFavIcecreamEl(index);
+  const locationEl = createLocationEl(index);
+  const userCommentEl = createUserCommentEl(index);
+
+  appendContentsUserImgBoxEl(userImgBoxEl, userImgEl);
+  appendUsernameContainerEl(index, usernameContainerEl, usernameEl, userLinkEl, favIcecreamEl);
+  appendUserTextBoxHeaderEl(userTextBoxHeaderEl, usernameContainerEl, locationEl);
+  appendUserTextBoxEl(userTextBoxEl, userTextBoxHeaderEl, userCommentEl);
+  appendScrimbaContributorsCardEl(scrimbaContributorsCardEl, userImgBoxEl, userTextBoxEl)
+  appendScrimbaContributorsContainerEl(scrimbaContributorsCardEl)
+}
+
+const renderScrimbaContributors = function () {
+  if (contributors.length <= loadAmount) {
+    scrimbaContributorsLoadMoreBtnEl.classList.add('hide')
+  }
+  for (let i = 0; i < loadAmount; i++) {
+    createScrimbaContributorsElements(i);
+  }
+  loadCount = document.querySelectorAll('.scrimba-contributors-card').length;
+  console.log(loadCount);
+}
+
+/* 
+    below is just a function that loads a specific amount of
+    contriutors per click
+*/
+scrimbaContributorsLoadMoreBtnEl.addEventListener('click', function () {
+  createMoreContributorsElements();
+  checkLoadCount();
+
+  console.log(loadCount);
+})
+
+const init = function () {
+  loadAmount = 4;
+  loadCount = document.querySelectorAll('.scrimba-contributors-card').length;
+  renderScrimbaContributors();
+}
+
+init()
--- assets/javascript/contributors.js
+++ assets/javascript/contributors.js
@@ -1,26 +1,49 @@
 const contributors = [
     {
         name: "Micha",
+        githubName: '',
         location: "Austin, TX, USA",
-        favorite_ice_cream: "dark chocolate"
+        favorite_ice_cream: "dark chocolate",
+        github: '',
+        profileImage: '',
+        likes: ''
     },
     {
         name: "Kevin Do",
+        githubName: '',
         location: "Florida, Fl, USA",
-        favorite_ice_cream: "Milk chocolate"
+        favorite_ice_cream: "Milk chocolate",
+        github: '',
+        profileImage: '',
+        likes: ''
     },
     {
         name: "Ali",
+        githubName: '',
         location: "Global Citizen",
-        favorite_ice_cream: "Coffee Coffee BuzzBuzzBuzz"
+        favorite_ice_cream: "Coffee Coffee BuzzBuzzBuzz",
+        github: '',
+        profileImage: '',
+        likes: ''
     },
     {
-        name: "awcoetzer",
-        location: "Bulgaria, Sofia",
-        favorite_ice_cream: [
-            'Vanilla',
-            'Rocky road'
-        ]
+        name: "Andre",
+        githubName: 'awcoetzer',
+        location: "Sofia, Bulgaria",
+        favorite_ice_cream: 'vanilla',
+        githubLink: 'https://github.com/awcoetzer',
+        profileImage: 'https://avatars.githubusercontent.com/u/83278375?v=4',
+        likes: 'I love Scrimba because Scrimba helps you put theory into practice!, not to mention the amazing community is friendly and supportive.'
+    },
+    {
+        name: "Alpaca Smacka",
+        githubName: 'AlpacaSmacka3000',
+        location: "Sofia, Bulgaria",
+        favorite_ice_cream: 'Grass',
+        githubLink: 'https://github.com/awcoetzer',
+        profileImage: 'https://avatars.githubusercontent.com/u/83278375?v=4',
+        likes: 'Random alpaca words, eating grass and pouncing around like no one is watching'
     }
 ]
 
+export { contributors }
