d424e7364edebef492f14e0eb92162ba3580d105
awcoetzer <83278375+awcoetzer@users.noreply.github.com>
2023-06-20 12:25:05 +0300

    fix: temporary fix of color contrast
    Just disabled the the color contrast while its being fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,25 +2,24 @@
 
 const currentLocalTimeEl = document.getElementById('current-local-time')
 const greetingEl = document.querySelector('#greeting');
-console.log(greetingEl);
 
 setInterval(() => {
     const currentLocalTime = new Date().toLocaleTimeString();
     // the user is saving the date object to a variable
-	const date = new Date();
+    const date = new Date();
     // then pulling the hours from the date object
     const hours = date.getHours();
-    
+
     /* 
         user is declaring an undefined variable outside
         the scope of the control structure / if statement 
         below and then setting the value depending on
         the hours.
     */
-	let timeOfDay;
+    let timeOfDay;
     let timeOfDayEmote;
 
-	if (hours < 12) {
+    if (hours < 12) {
         timeOfDay = "morning";
         timeOfDayEmote = '🌄';
     } else if (hours >= 12 && hours < 17) {
@@ -29,11 +28,14 @@ setInterval(() => {
     } else {
         timeOfDay = "night";
         timeOfDayEmote = '🌆';
-        document.body.style.color = "silver";
-        document.body.style.background = "var(--clr-maroon-overload-will-s-205)";
+        /*
+            I stopped the change of colors until contrast is fixed
+            made it difficult to work with during the night
+        */
+        // document.body.style.color = "silver";
+        // document.body.style.background = "var(--clr-maroon-overload-will-s-205)";
     }
 
     currentLocalTimeEl.textContent = `Local Time: ${currentLocalTime}`;
     greetingEl.textContent = `Good ${timeOfDay} ${timeOfDayEmote} bootcamp students!`
 }, 1000)
-
