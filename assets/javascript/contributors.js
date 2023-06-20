f2e8905079be939503bcc1b84a2f51c143bc6f05
awcoetzer <83278375+awcoetzer@users.noreply.github.com>
2023-06-20 14:11:05 +0300

    fix: remove test contributors and placed example

--- assets/javascript/contributors.js
+++ assets/javascript/contributors.js
@@ -1,3 +1,14 @@
+/*
+    EXAMPLE:
+        name: "Andre",
+        githubName: 'awcoetzer',
+        location: "Sofia, Bulgaria",
+        favorite_ice_cream: 'vanilla',
+        githubLink: 'https://github.com/awcoetzer',
+        profileImage: 'https://avatars.githubusercontent.com/u/83278375?v=4',
+        likes: 'I love Scrimba because Scrimba helps you put theory into practice!, not to mention the amazing community is friendly and supportive.'
+*/
+
 const contributors = [
     {
         name: "Micha",
@@ -34,51 +45,6 @@ const contributors = [
         githubLink: 'https://github.com/awcoetzer',
         profileImage: 'https://avatars.githubusercontent.com/u/83278375?v=4',
         likes: 'I love Scrimba because Scrimba helps you put theory into practice!, not to mention the amazing community is friendly and supportive.'
-    },
-    {
-        name: "Alpaca Smacka",
-        githubName: 'AlpacaSmacka3000',
-        location: "Sofia, Bulgaria",
-        favorite_ice_cream: 'Grass',
-        githubLink: 'https://github.com/awcoetzer',
-        profileImage: '',
-        likes: 'Random alpaca words, eating grass and pouncing around like no one is watching'
-    },
-    {
-        name: "Alpaca Smacka",
-        githubName: 'AlpacaSmacka3000',
-        location: "Sofia, Bulgaria",
-        favorite_ice_cream: 'Grass',
-        githubLink: 'https://github.com/awcoetzer',
-        profileImage: '',
-        likes: 'Random alpaca words, eating grass and pouncing around like no one is watching'
-    },
-    {
-        name: "Alpaca Smacka",
-        githubName: 'AlpacaSmacka3000',
-        location: "Sofia, Bulgaria",
-        favorite_ice_cream: 'Grass',
-        githubLink: 'https://github.com/awcoetzer',
-        profileImage: '',
-        likes: 'Random alpaca words, eating grass and pouncing around like no one is watching'
-    },
-    {
-        name: "Alpaca Smacka",
-        githubName: 'AlpacaSmacka3000',
-        location: "Sofia, Bulgaria",
-        favorite_ice_cream: 'Grass',
-        githubLink: 'https://github.com/awcoetzer',
-        profileImage: '',
-        likes: 'Random alpaca words, eating grass and pouncing around like no one is watching'
-    },
-    {
-        name: "Alpaca Smacka",
-        githubName: 'AlpacaSmacka3000',
-        location: "Sofia, Bulgaria",
-        favorite_ice_cream: 'Grass',
-        githubLink: 'https://github.com/awcoetzer',
-        profileImage: '',
-        likes: 'Random alpaca words, eating grass and pouncing around like no one is watching'
     }
 ]
 
