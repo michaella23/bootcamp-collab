e439a441375fdf32abfef730714acb9e4768be31
Michaella Rodriguez <micha.ella@hotmail.com>
2023-06-21 13:20:37 -0500

    Added more info to `contributors.js`, moved down

--- assets/javascript/contributors.js
+++ assets/javascript/contributors.js
@@ -20,15 +20,6 @@ const contributors = [
         likes: 'Everything that involves building new things'
     },
     {
-        name: "Micha",
-        githubName: '',
-        location: "Austin, TX, USA",
-        favorite_ice_cream: "dark chocolate",
-        github: '',
-        profileImage: '',
-        likes: ''
-    },
-    {
         name: "Kevin Do",
         githubName: '',
         location: "Florida, Fl, USA",
@@ -54,6 +45,15 @@ const contributors = [
         githubLink: 'https://github.com/awcoetzer',
         profileImage: 'https://avatars.githubusercontent.com/u/83278375?v=4',
         likes: 'I love Scrimba because Scrimba helps you put theory into practice!, not to mention the amazing community is friendly and supportive.'
+    },
+    {
+        name: "Micha",
+        githubName: 'michaella23',
+        location: "Austin, TX, USA",
+        favorite_ice_cream: "dark chocolate",
+        github: 'https://github.com/michaella23',
+        profileImage: 'https://avatars.githubusercontent.com/u/89760083?s=400&u=6bb83282490c6231089deb3d16d1a8c57bfa6d83&v=4',
+        likes: 'Scrimba Bootcamp rocks! I love the community and all the amazing projects people are building.'
     },
 ]
 
