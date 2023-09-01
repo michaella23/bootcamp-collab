b07c8d4525e6f356ef3c6c6f1c585dbc3a49fc28
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-09-01 17:08:02 +0200

    [#139] Added 2 projects

--- assets/javascript/open-source-projects.js
+++ assets/javascript/open-source-projects.js
@@ -12,7 +12,19 @@ const openSourceProjects = [
     description: "freeCodeCamp.org is a friendly community where you can learn to code for free.",
     githubURL: 'https://github.com/freeCodeCamp/freeCodeCamp',
     liveURL: 'https://www.freecodecamp.org/'
-  }
+  },
+  {
+    name: 'Before I Die',
+    description: "An open source project that helps developers make their first pull request and contribute to open source projects. 🚀🎯 Developers can contribute by sharing what they want to do before they die. 💡",
+    githubURL: 'https://github.com/BeforeIDieCode/BeforeIDieAchievements',
+    liveURL: 'https://before-i-die-achievements.vercel.app/'
+  },
+  {
+    name: 'first-contributions',
+    description: "🚀✨ Help beginners to contribute to open source projects",
+    githubURL: 'https://github.com/firstcontributions/first-contributions',
+    liveURL: 'https://firstcontributions.github.io/'
+  },
 ]
 
 export { openSourceProjects }
